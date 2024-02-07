22e2b9b122a47ece67eaa568d99628457b696b7a
RABOTEAU Pierre <PRABOTEAU@micropole.com>
2024-02-07 17:01:41 +0100

    add config

--- src/main/java/com/example/demo/adapter/in/web/CharacterController.java
+++ src/main/java/com/example/demo/adapter/in/web/CharacterController.java
@@ -13,12 +13,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 public class CharacterController {
 
     private final CharacterUseCase characterUseCase;
@@ -26,17 +26,15 @@ public class CharacterController {
     @PostMapping("/characters")
     public ResponseEntity<Character> postCharacter(@RequestBody Character character, UriComponentsBuilder ucb) {
         Character response = this.characterUseCase.createCharacter(character);
-        
+
         return ResponseEntity.created(ucb.path("characters/{id}")
-                                    .buildAndExpand(response.id()).toUri())
-                                    .body(response);
+                .buildAndExpand(response.id()).toUri())
+                .body(response);
     }
 
     @GetMapping("/characters")
     public ResponseEntity<List<Character>> findCharacters() {
         return ResponseEntity.ok(this.characterUseCase.findCharacter());
     }
-    
-    
-    
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into character (nickname, level) Values ('Meraxes', 200);
+insert into character (nickname, level) Values ('Hehrys', 200);
+insert into character (nickname, level) Values ('Merax-elio', 200);
+insert into character (nickname, level) Values ('Merax-steam', 200);
